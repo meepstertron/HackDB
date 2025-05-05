f2ae2f755b066c318f963b980adcbd1681f78ed0
meepstertron <meepstertron@gmail.com>
2025-05-05 07:55:28 +0200

    Started working on frontend adding a simple sidebar and headder layout :)

--- frontend/src/components/layout.tsx
+++ frontend/src/components/layout.tsx
@@ -5,11 +5,11 @@ function RootLayout({ children }: React.PropsWithChildren) {
         <div className="h-screen w-screen flex bg-gray-100 text-gray-900">
             <Sidebar />
             <div className="flex flex-col flex-1">
-                {/* Header */}
+                
                 <header className="h-16 flex items-center px-6 border-b bg-white">
                     <h1 className="text-xl font-semibold">Header</h1>
                 </header>
-                {/* Main Content */}
+                
                 <main className="flex-1 p-6 overflow-auto">
                     {children}
                 </main>
--- frontend/src/components/sidebar.tsx
+++ frontend/src/components/sidebar.tsx
@@ -1,24 +1,26 @@
 import { Database, Hash, PercentDiamondIcon, Settings2, Workflow } from "lucide-react";
+// use react router dom to navigate between pages
+import { useNavigate } from "react-router-dom";
 
 function Sidebar() {
-
+    const navigate = useNavigate();
     const elements = [
         { name: "Databases", icon: <Database />, location: "/databases", active: true },
         { name: "Workflows", icon: <Workflow />, location: "/workflows", active: false },
         { name: "Quota", icon: <PercentDiamondIcon />, location: "/quota", active: false },
         { name: "Tokens", icon: <Hash />, location: "/tokens", active: false },
-        { name: "Settings", icon: <Settings2 />, location: "/settings", active: false },
+        { name: "Settings", icon: <Settings2 />, location: "/settings", active: true },
     ];
 
     return ( 
         <aside className="w-16 h-full bg-white border-r flex flex-col items-center p-4">
-            {/* Logo at the top */}
+            
             <div className="mb-8">
                 <img src="/logo192.png" alt="Logo" className="h-8 w-8" />
             </div>
-            <ul className="flex flex-col items-center w-full text-gray-600">
+            <ul className="flex flex-col items-center w-full text-gray-800">
                 {elements.map((element) => (
-                <li className="flex flex-col items-center gap-1 py-2" onClick={ () => }>
+                <li className={`flex flex-col items-center gap-1 py-2   w-16 ${!element.active ? "text-gray-500 cursor-auto" : "cursor-pointer hover:bg-gray-100"}`} onClick={ () => { if (element.active) { navigate(element.location) } } } key={element.name}>
                     {element.icon}
                     <span className="text-xs mt-1">{element.name}</span>
                 </li>
