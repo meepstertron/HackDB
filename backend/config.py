71ceae24a5bbf850a1026136d2327260f59fd2d7
meepstertron <meepstertron@gmail.com>
2025-07-05 14:34:54 +0200

    fix: update default DB host to 'hackdb-db' for consistency

--- backend/config.py
+++ backend/config.py
@@ -7,7 +7,7 @@ class Config:
     db_user = os.environ.get("POSTGRES_USER")
     db_password = os.environ.get("POSTGRES_PASSWORD")
     db_name = os.environ.get("POSTGRES_DB")
-    db_host = os.environ.get("DB_HOST", "db")
+    db_host = os.environ.get("DB_HOST", "hackdb-db")
     db_port = os.environ.get("DB_PORT", "5432")
     SQLALCHEMY_DATABASE_URI = f'postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
